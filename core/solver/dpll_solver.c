c346df5e897e1a09ac1e4c8fe8d2a68d0794f1ed
Tom Kiesow <tom@kiesow.email>
2022-01-22 14:27:26 +0100

    implement dpll_solver

--- core/solver/dpll_solver.c
+++ core/solver/dpll_solver.c
@@ -1,2 +1,59 @@
 
 #include "solver/dpll_solver.h"
+
+
+int dpll_solve(cnf *pCnf, int32_t (*pickAndRemove)(cnf *), assignment *pAssignmentResult) {
+    assignment emptyAssignment;
+    assignment_create(&emptyAssignment);
+
+    return dpll_solve_partial(pCnf, &emptyAssignment, pickAndRemove, pAssignmentResult);
+}
+
+int
+dpll_solve_partial(cnf *pCnf, assignment *pAssignment, int32_t (*pickAndRemove)(cnf *), assignment *pAssignmentResult) {
+    cnf simplified;
+    cnf_create(&simplified);
+
+    // simplify (pCnf, pAssignment, &simplified);
+
+    // if cnf is empty -> sat
+    if (pCnf->count == 0u) {
+        *pAssignmentResult = *pAssignment;
+        return 0;
+    }
+
+    // if cnf contains empty clause -> unsat
+    cnf_clause_iterator iter;
+    cnf_clause_iterator_create(&iter, pCnf);
+
+    while (cnf_clause_iterator_next(&iter)) {
+        if (iter.count == 0){
+            return 1;
+        }
+    }
+
+    int32_t nextLiteral = pickAndRemove(pCnf);
+    uint32_t nextVariable = nextLiteral > 0 ? nextLiteral : -nextLiteral;
+
+    // assignment with nextLiteral evaluating to false
+    assignment pFalseAssigment;
+    assignment_copy(&pFalseAssigment, pAssignment);
+
+    // for the assignment where nextLiteral evaluates to false we just reuse pAssignment
+
+    // set value of nextVariable in pAssignment, so that it evaluates to true
+    assignment_set(pAssignment, nextVariable, (nextLiteral > 0) ? 1 : 0);
+
+    // set value of nextVariable in pFalseAssigment, so that it evaluates to false
+    assignment_set(&pFalseAssigment, nextVariable, (nextLiteral < 0) ? 1 : 0);
+
+    if (!dpll_solve_partial(&simplified, pAssignment, pickAndRemove, pAssignmentResult)){
+        return 0;
+    }
+
+    if (!dpll_solve_partial(pCnf, &pFalseAssigment, pickAndRemove, pAssignmentResult)){
+        return 0;
+    }
+
+    return 1;
+}
--- core/solver/solver/dpll_solver.h
+++ core/solver/solver/dpll_solver.h
@@ -3,9 +3,12 @@
 #define DPLLSOLVER_DPLL_SOLVER_H
 
 #include "solver.h"
+#include "solver/assignment.h"
 
 #include <cnf/cnf.h>
 
-void dpll_solve(solver* solver, cnf* cnf);
+int dpll_solve(cnf* pCnf, int32_t (*pickAndRemove)(cnf *), assignment* pAssignmentResult);
+
+int dpll_solve_partial(cnf *pCnf, assignment *pAssignment, int32_t (*pickAndRemove)(cnf *), assignment *pAssignmentResult);
 
 #endif
