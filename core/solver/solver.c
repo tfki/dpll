75d1a334927845e8a3ed1205dded67a6169d362d
Tom Kiesow <tom@kiesow.email>
2022-01-25 20:55:02 +0100

    remove ClauseBuffer from solver.h

--- core/solver/solver.c
+++ core/solver/solver.c
@@ -2,66 +2,6 @@
 
 #include <common/common.h>
 
-typedef struct ClauseBuffer
-{
-  int32_t* pData;
-  size_t capacity;
-  size_t count;
-} ClauseBuffer;
-
-static inline int
-ClauseBuffer_create(ClauseBuffer* pClauseBuffer)
-{
-  SANITIZING_ASSERT(pClauseBuffer); // pClauseBuffer must be a valid pointer
-
-  pClauseBuffer->count = 0u;
-  pClauseBuffer->capacity = 1024u;
-  pClauseBuffer->pData = malloc(pClauseBuffer->capacity * sizeof(int32_t));
-
-  if (!pClauseBuffer->pData)
-    return 1;
-
-  return 0;
-}
-
-static inline int
-ClauseBuffer_push(ClauseBuffer* pClauseBuffer, int32_t literal)
-{
-  SANITIZING_ASSERT(pClauseBuffer); // pClauseBuffer must be a valid pointer
-
-  if (pClauseBuffer->count + 1u > pClauseBuffer->capacity) {
-
-    int32_t* pNewData = realloc(pClauseBuffer->pData, pClauseBuffer->capacity * 2u * sizeof(int32_t));
-    if (!pNewData)
-      return 1;
-
-    pClauseBuffer->pData = pNewData;
-    pClauseBuffer->capacity *= 2u;
-  }
-
-  pClauseBuffer->pData[pClauseBuffer->count] = literal;
-  ++pClauseBuffer->count;
-
-  return 0;
-}
-
-static inline void
-ClauseBuffer_destroy(ClauseBuffer* pClauseBuffer)
-{
-  SANITIZING_ASSERT(pClauseBuffer); // pClauseBuffer must be a valid pointer
-
-  free(pClauseBuffer->pData);
-  pClauseBuffer->pData = NULL;
-  pClauseBuffer->capacity = 0u;
-  pClauseBuffer->count = 0u;
-}
-
-static inline void
-ClauseBuffer_reset(ClauseBuffer* pClauseBuffer)
-{
-  SANITIZING_ASSERT(pClauseBuffer); // pClauseBuffer must be a valid pointer
-  pClauseBuffer->count = 0u;
-}
 
 int
 Cnf_simplify(const Cnf* pCnf, const Assignment* pAssignment, Cnf* pNextCnf)
