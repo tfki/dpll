71f91604ebfd0085fe3668916256ec4c67ceafac
Robert Andreas Fritsch <info@robert-fritsch.de>
2022-01-26 15:58:58 +0100

    Added a copy target for the Test resources and cleaned up SANITIZING_ASSERT a little

--- core/common/common/common.h
+++ core/common/common/common.h
@@ -1,21 +1,30 @@
 #ifndef DPLLSOLVER_COMMON_H
 #define DPLLSOLVER_COMMON_H
 
-#include <stdio.h>
-
-#ifdef NDEBUG // TODO replace with SANITIZING compiler flag if
-              //      the performance drops to much in debug mode
+#ifdef NDEBUG
 
 #define SANITIZING_ASSERT(c) ((void)0)
 
 #else
 
+#ifdef _WIN32
+#include <windows.h>
 #define SANITIZING_ASSERT(c)                                                                                                                                   \
   if (!(c)) {                                                                                                                                                  \
-    printf("assert failed in file %s on line %d", __FILE__, __LINE__);                                                                                         \
-    exit(-1);                                                                                                                                                  \
+    DebugBreak();                                                                                                                                              \
+    ExitProcess(__LINE__);                                                                                                                                     \
   }                                                                                                                                                            \
   ((void)0)
+
+#else
+
+#define SANITIZING_ASSERT(c)                                                                                                                                   \
+  if (!(c)) {                                                                                                                                                  \
+    exit(__LINE__);                                                                                                                                            \
+  }                                                                                                                                                            \
+  ((void)0)
+
+#endif
 
 #endif
 
